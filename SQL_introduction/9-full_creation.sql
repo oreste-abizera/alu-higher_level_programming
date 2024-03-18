03f85c399df6da625608dea1640e71407a78faf8
oreste-abizera <oresteabizera11@gmail.com>
2024-03-18 17:16:19 +0200

    Add SQL query to retrieve top scores from second_table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- script that creates a table second_table in the database hbtn_0c_0 in MySQL server and add multiples rows.
+-- creates a table second_table in the database hbtn_0c_0 in MySQL server and add multiples rows.
 CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name) VALUES (2, 'Alex', 3);
