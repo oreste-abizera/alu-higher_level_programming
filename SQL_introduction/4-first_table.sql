6595cbc1cf369c2524dffc430925fc18f7fdc4b3
oreste-abizera <oresteabizera11@gmail.com>
2024-03-18 17:03:41 +0200

    Add first_table to the database

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- creates a table called first_table in the current database in MySQL server.
+CREATE TABLE first_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT
+);
