6b89f39dc5770a1ae27c547d0ca09b463fe99b92
oreste-abizera <oresteabizera11@gmail.com>
2024-03-18 17:09:23 +0200

    Add SQL script to insert a new row in first_table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- creates a table called first_table in the current database in MySQL server.
 CREATE TABLE first_table (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    age INT
+    name VARCHAR(100)
 );
