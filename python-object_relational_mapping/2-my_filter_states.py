6406376f183ea76aee38ade1a7a7e89f43b6303e
oreste-abizera <oresteabizera11@gmail.com>
2024-04-07 13:49:02 +0200

    Add script to filter and print states starting with 'N' from the database

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N (upper N)
-from the database
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
 """
 import MySQLdb
 from sys import argv
 
 # The code should not be executed when imported
 if __name__ == '__main__':
+
     # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
@@ -15,9 +16,8 @@ if __name__ == '__main__':
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name\
-                LIKE BINARY 'N%' ORDER BY id ASC")
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cur.execute(nmeSr)
 
     rows = cur.fetchall()
     for i in rows:
